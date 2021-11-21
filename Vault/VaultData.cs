89007774eeb0053f5f3d003d2c099481e15c6e9d
Adrian Haug <75075584+Zodiaknight@users.noreply.github.com>
2021-11-21 16:06:30 +0100

    Add files via upload

---
+++ Vault/VaultData.cs
@@ -0,0 +1,287 @@
+﻿using System.Collections.Generic;
+using System;
+using Core.Traps;
+using Core.Inventory;
+using Core.Enums;
+using Core.Level;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Core.Data
+{
+    [Serializable]
+    public class VaultData
+    {
+        public List<TrapData> trapInfos = new List<TrapData>();
+        public List<MaterialData> materialInfos = new List<MaterialData>();
+
+        #region Events
+        public event EventHandler<VaultTrapEventArgs>       VaultTrapChanged;
+        public event EventHandler<VaultMaterialEventArgs>   VaultMaterialChanged;
+        public event EventHandler<VaultNutsEventArgs>       VaultNutsChanged;
+
+        private void OnVaultTrapChanged(VaultTrapEventArgs vaultChange)
+        {
+            Debug.Log("Event Trap Change");
+            VaultTrapChanged?.Invoke(this, vaultChange);
+        }
+
+        private void OnVaultMaterialChanged(VaultMaterialEventArgs vaultChange)
+        {
+            Debug.Log("Event Material Change");
+            VaultMaterialChanged?.Invoke(this, vaultChange);
+        }
+
+        private void OnVaultNutsChanged(VaultNutsEventArgs vaultChange)
+        {
+            Debug.Log("Event Nuts Change");
+            VaultNutsChanged?.Invoke(this, vaultChange);
+        }
+        #endregion
+
+        #region Traps
+        public void AddTrap(TrapData newTrap)
+        {
+            trapInfos.Add(newTrap);
+
+            VaultTrapEventArgs vaultChange = new VaultTrapEventArgs(VaultOp.Add, newTrap); // change-event
+            OnVaultTrapChanged(vaultChange);
+        }
+
+        public TrapData AddNewTrap(EnumManager.Traps trapType) // adds "brand new trap" = full durability.
+        {
+            TrapData trapToAdd = ReferenceManager.Instance.trapManager.GetTrapDataById(trapType);
+            if (trapToAdd == null) return null;
+
+            AddTrap(trapToAdd);
+            return trapToAdd; // return the new trap to allow modification
+        }
+
+        public void UpdateTrap(TrapData trap)
+        {
+            DeleteTrap(trap); // delete an send delete event
+            AddTrap(trap); // re add and send add event -> results in "update"
+            // "change" event type is never used for traps...
+        }
+
+        public bool DeleteTrap(TrapData trapDataDelete)
+        {
+            int i = trapInfos.FindIndex(d => d == trapDataDelete);
+            if (i == -1) return false;
+
+            trapInfos.RemoveAt(i);
+
+            VaultTrapEventArgs vaultChange = new VaultTrapEventArgs(VaultOp.Delete, trapDataDelete); // change-event
+            OnVaultTrapChanged(vaultChange);
+            return true;
+        }
+
+        public void SellTrap(TrapData trapDataSell)
+        {
+            if (!DeleteTrap(trapDataSell)) return;
+
+            float percent = trapDataSell.percentDurability;
+            UpdateNutAmount(CalculateTrapLossOfValue(percent, trapDataSell.nutPrice));
+        }
+
+        public void DismantleTrap(TrapData trapData)
+        {
+            float percent = trapData.percentDurability;
+
+            foreach (var requiredMaterial in trapData.requiredMaterials)
+            {
+                AddMaterial(requiredMaterial.material, CalculateTrapLossOfValue(percent, requiredMaterial.count));
+            }
+            DeleteTrap(trapData);
+        }
+
+        private int CalculateTrapLossOfValue(float percent, int originalAmount, float maxValuePercent = 0.8f, int minReturnValue = 1)
+        {
+            if (percent > maxValuePercent) percent = maxValuePercent;
+            int amount = Mathf.FloorToInt(originalAmount * percent);
+            if (amount < minReturnValue) amount = minReturnValue; // always return at least one
+            return amount;
+        }
+
+        #endregion
+
+        #region Material
+        public MaterialData GetMaterialData(EnumManager.Materials matType)
+        {
+            for (int i = 0; i < materialInfos.Count; i++)
+            {
+                if (matType == materialInfos[i].type)
+                {
+                    return materialInfos[i];
+                }
+            }
+            return null;
+        }
+
+        public void AddMaterial(MaterialData newMaterial)
+        {
+            UpdateMaterial(newMaterial);
+        }
+
+        public void AddMaterial(EnumManager.Materials matType, int amount = 1)
+        {
+            MaterialData materialToAdd = ReferenceManager.Instance.materialManager.ConfigureNewMaterial(matType);
+            materialToAdd.amount = amount;
+            UpdateMaterial(materialToAdd);
+        }
+
+        public void UpdateMaterial(MaterialData newMaterial)
+        {
+            bool addedToExisting = false;
+            for (int i = 0; i < materialInfos.Count; i++)
+            {
+                if (materialInfos[i].type == newMaterial.type)
+                {
+                    ChangeMaterialAmount(newMaterial, materialInfos[i]);
+                    addedToExisting = true;
+                    break;
+                }
+            }
+            if (!addedToExisting)
+            {
+                materialInfos.Add(newMaterial);
+                VaultMaterialEventArgs vaultChange = new VaultMaterialEventArgs(VaultOp.Add, newMaterial); // change-event
+                OnVaultMaterialChanged(vaultChange);
+            }
+        }
+
+        public void UseMaterial(RequiredMaterialData requiredMaterial)
+        {
+            MaterialData materialToUse = ReferenceManager.Instance.materialManager.ConfigureNewMaterial(requiredMaterial.material);
+            materialToUse.amount = -requiredMaterial.count;
+            UpdateMaterial(materialToUse);
+        }
+
+        private void ChangeMaterialAmount(MaterialData newMaterial, MaterialData existingMaterial)
+        {
+            existingMaterial.amount += newMaterial.amount;
+
+            if (existingMaterial.amount <= 0)
+            {
+                existingMaterial.amount = 0;
+                DeleteMaterial(existingMaterial);
+                return;
+            }
+
+            VaultMaterialEventArgs vaultChange = new VaultMaterialEventArgs(VaultOp.Change, existingMaterial); // change-event
+            OnVaultMaterialChanged(vaultChange);
+        }
+
+        private void DeleteMaterial(MaterialData material)
+        {
+            materialInfos.Remove(material);
+            VaultMaterialEventArgs vaultChange = new VaultMaterialEventArgs(VaultOp.Delete, material); // change-event
+            OnVaultMaterialChanged(vaultChange);
+        }
+
+        public void SellMaterial(MaterialData materialSellData, int amount)
+        {
+            MaterialData material = GetMaterialData(materialSellData.type);
+
+            materialSellData = materialSellData.ShallowCopy();
+
+            if ((material == null) || (material.amount <= 0)) return;
+            if (material.amount < amount) amount = material.amount;
+
+            materialSellData.amount = -amount;
+
+            UpdateMaterial(materialSellData);
+
+            int sumNuts = materialSellData.nutPrice * amount;
+            UpdateNutAmount(sumNuts);
+        }
+
+        public void SellMaterial(MaterialData materialSellData) // materialData positive amount of amount to sell
+        {
+            MaterialData material = GetMaterialData(materialSellData.type);
+
+            if ((material == null) || (material.amount <= 0)) return;
+            if (material.amount < materialSellData.amount) materialSellData.amount = material.amount;
+
+            int sumNuts = materialSellData.nutPrice * materialSellData.amount;
+            UpdateNutAmount(sumNuts);
+
+            materialSellData.amount = -materialSellData.amount;
+            UpdateMaterial(materialSellData);
+        }
+
+        public int GetSumOfAllMaterialAmounts()
+        {
+            int amount = 0;
+            foreach (MaterialData material in materialInfos)
+            {
+                amount += material.amount;
+            }
+            return amount;
+        }
+
+        public bool CheckIfEnoughMaterials(EnumManager.Materials matType, int amountNeeded)
+        {
+            for (int i = 0; i < materialInfos.Count; i++)
+            {
+                if (materialInfos[i].type == matType && materialInfos[i].amount >= amountNeeded) return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Nuts
+        public int GetNutAmount() // get current nuts
+        {
+            return ReferenceManager.Instance.dataManager.playerData.nutAmount;
+        }
+
+        public void UpdateNutAmount(int nuts) // handles relative +/- nuts
+        {
+            ReferenceManager.Instance.dataManager.playerData.nutAmount += nuts;
+
+            VaultNutsEventArgs vaultChange = new VaultNutsEventArgs(VaultOp.Change, ReferenceManager.Instance.dataManager.playerData.nutAmount); // change-event
+            OnVaultNutsChanged(vaultChange);
+        }
+
+        public void SetNutAmount(int nuts)
+        {
+            ReferenceManager.Instance.dataManager.playerData.nutAmount = nuts;
+
+            VaultNutsEventArgs vaultChange = new VaultNutsEventArgs(VaultOp.Change, ReferenceManager.Instance.dataManager.playerData.nutAmount); // change-event
+            OnVaultNutsChanged(vaultChange);
+        }
+        #endregion
+
+        #region Editor Buttons
+        [EnumToggleButtons]
+        public EnumManager.Traps enumTraps;
+
+        [Button]
+        public void AddOneTrapToVault()
+        {
+            AddNewTrap(enumTraps);
+        }
+
+        [Button]
+        public void AddAllTraps()
+        {
+            foreach (EnumManager.Traps item in Enum.GetValues(typeof(EnumManager.Traps)))
+            {
+                AddNewTrap(item);
+            }
+        }
+
+        [Button]
+        public void ClearAllTraps()
+        {
+            foreach (var item in trapInfos)
+            {
+                VaultTrapEventArgs vaultChange = new VaultTrapEventArgs(VaultOp.Delete, item); // change-event
+                OnVaultTrapChanged(vaultChange);
+            }
+            trapInfos.Clear();
+        }
+        #endregion
+    }
+}
---
+++ Vault/VaultPanel.cs
@@ -0,0 +1,192 @@
+﻿using Core.Crafting;
+using Core.Enums;
+using Core.Inventory;
+using Core.Level;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Core.Data;
+using Core.Gameflow;
+using Sirenix.OdinInspector;
+
+namespace Core.UI
+{
+    public class VaultPanel : MonoBehaviour
+    {
+        public Transform trapSlotHolder;
+        public Transform materialSlotHolder;
+        public Transform nutAmount;
+
+        public int sellPercent = 10;
+
+        public EnumManager.Materials selectedMaterialId;
+        public CraftingMenu craftingMenu;
+        private SlotSelectionHandler materialSelectionHandler;
+        private TrapSlotListHandler trapListHandler;
+        private SlotTrap selectedTrapSlot;
+        public List<SlotMaterial> materialSlots;
+
+        private VaultData vaultData = null;
+        public static VaultPanel Instance { get; private set; }
+
+        private void Awake()
+        {
+            vaultData = ReferenceManager.Instance.dataManager.playerData.vault;
+            trapListHandler = new TrapSlotListHandler(trapSlotHolder.transform, new Vector3(1f, 1f, 1f));
+            materialSelectionHandler = new SlotSelectionHandler();
+
+            Instance = this;
+            LoadTraps();
+            LoadMaterials();
+
+            //Events zuweisung
+            vaultData.VaultTrapChanged      += ReceivedVaultTrapChange;
+            vaultData.VaultMaterialChanged  += ReceivedVaultMaterialChange;
+            vaultData.VaultNutsChanged      += ReceivedNutChange;
+
+            trapListHandler.SelectionHandler.SlotSelectChanged  += ReceivedTrapSelectionChange;
+            materialSelectionHandler.SlotSelectChanged          += ReceivedMaterialSelectionChange;
+        }
+
+        private void OnEnable()
+        {
+            UpdateNutAmount(vaultData.GetNutAmount());
+        }
+
+        public void OnDisable()
+        {
+            trapListHandler.SelectionHandler.Deselect();
+
+            if (GameManager.Instance.currentScene == GameManager.Scenes.Overworld)
+            {
+                LevelInfoScreen.Instance.gameObject.SetActive(true);
+            }
+        }
+
+        #region Events
+        public void ReceivedVaultTrapChange(object sender, VaultTrapEventArgs vaultChange)
+        {
+            switch (vaultChange.Operation)
+            {
+                case VaultOp.Add: AddTrap(vaultChange.TrapData); break;
+                case VaultOp.Delete: RemoveTrap(vaultChange.TrapData); break;
+                case VaultOp.Change: break; // maybe update durability
+            };
+        }
+
+        public void ReceivedVaultMaterialChange(object sender, VaultMaterialEventArgs vaultChange)
+        {
+            switch (vaultChange.Operation)
+            {
+                case VaultOp.Add: UpdateMaterialAmount(vaultChange.MaterialData.type, vaultChange.MaterialData.amount); break;
+                case VaultOp.Delete: UpdateMaterialAmount(vaultChange.MaterialData.type, 0); break;
+                case VaultOp.Change: UpdateMaterialAmount(vaultChange.MaterialData.type, vaultChange.MaterialData.amount); break;
+            }
+        }
+
+        private void ReceivedNutChange(object sender, VaultNutsEventArgs vaultChange)
+        {
+            UpdateNutAmount(vaultChange.Nuts);
+        }
+
+        public void ReceivedTrapSelectionChange(object sender, SelectEventArgs selectionChange)
+        {
+            selectedTrapSlot = (SlotTrap)selectionChange.Slot;
+        }
+
+        public void ReceivedMaterialSelectionChange(object sender, SelectEventArgs selectionChange)
+        {
+            selectedMaterialId = ((SlotMaterial)selectionChange.Slot)?.materialType ?? EnumManager.Materials.None;
+        }
+        #endregion
+
+        #region Traps
+        private void LoadTraps()
+        {
+            trapListHandler.Load();
+        }
+
+        private void AddTrap(TrapData trapData)
+        {
+            trapListHandler.AddTrapToStack(trapData);
+        }
+
+        private void RemoveTrap(TrapData trapData)
+        {
+            trapListHandler.RemoveTrap(trapData);
+        }
+
+        public void SellTrap()
+        {
+            if (selectedTrapSlot != null) vaultData.SellTrap(selectedTrapSlot.trap);
+        }
+
+        public void DismantleTrap()
+        {
+            if (selectedTrapSlot != null) vaultData.DismantleTrap(selectedTrapSlot.trap);
+        }
+        #endregion
+
+        #region Material
+        private void LoadMaterials()
+        {
+            foreach (SO_Material mat in MaterialManager.Instance.materials)
+            {
+                EnumManager.Materials matType = mat.data.type;
+
+                SlotMaterial newSlot = (SlotMaterial)SlotManager.Instance.SpawnSlot(SlotType.MaterialSlot, materialSlotHolder);
+                newSlot.name = matType.ToString();
+                newSlot.materialType = matType; // Enum material type
+                newSlot.material = MaterialManager.Instance.GetMaterialData(matType); // template materialdata - not from vault, not unique
+                newSlot.SelectionHandler = materialSelectionHandler;
+                newSlot.gameObject.SetActive(true);
+                materialSlots.Add(newSlot);
+                newSlot.imageIcon.GetComponent<Image>().sprite = mat.data.sprite;
+
+                if (vaultData.GetMaterialData(matType) != null)
+                {
+                    newSlot.amountText.GetComponent<TMP_Text>().text = vaultData.GetMaterialData(matType).amount.ToString();
+                }
+                else
+                {
+                    newSlot.amountText.GetComponent<TMP_Text>().text = "0";
+                }
+            }
+        }
+
+        public void ShowMaterials()
+        {
+            foreach (Slot slot in materialSlots) { slot.gameObject.SetActive(true); }
+        }
+
+        [Button]
+        public void UpdateMaterialAmount(EnumManager.Materials mat, int newAmount)
+        {
+            foreach (SlotMaterial material in materialSlots)
+            {
+                if (material.materialType == mat)
+                {
+                    material.amountText.GetComponent<TMP_Text>().text = newAmount.ToString();
+                }
+            }
+        }
+
+        public void SellMaterial()
+        {
+            if (selectedMaterialId == EnumManager.Materials.None) return;
+            int amount = 1;
+            MaterialData matData = MaterialManager.Instance.ConfigureNewMaterial(selectedMaterialId);
+            matData.amount = amount;
+            if (matData != null) vaultData.SellMaterial(matData);
+        }
+        #endregion
+
+        #region Nuts
+        public void UpdateNutAmount(int amount)
+        {
+            nutAmount.GetComponent<TMP_Text>().text = amount.ToString();
+        }
+        #endregion
+    }
+}
